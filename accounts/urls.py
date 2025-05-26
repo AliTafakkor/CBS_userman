fb79b9fdec795e997aedee63709d5303f6138522
Ali <alitafakkor@gmail.com>
2025-05-26 15:26:20 -0400

    Project cleanup: add migrations, URLs, pyproject.toml, and initial structure

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    DepartmentViewSet, PrincipalInvestigatorViewSet, 
-    SponsoredUserViewSet, UserChangeRecordViewSet,
-    TestLoginView, TestCreateUserView  # New views for testing
-)
-
-router = DefaultRouter()
-router.register(r'departments', DepartmentViewSet)
-router.register(r'principal-investigators', PrincipalInvestigatorViewSet)
-router.register(r'sponsored-users', SponsoredUserViewSet)
-router.register(r'change-records', UserChangeRecordViewSet)
+from django.urls import path
+from .views import TestLoginView, TestCreateUserView  # Only import implemented views
 
 urlpatterns = [
-    path('', include(router.urls)),
     # Testing endpoints - remove in production
     path('test-login/', TestLoginView.as_view(), name='test-login'),
     path('test-create-user/', TestCreateUserView.as_view(), name='test-create-user'),
