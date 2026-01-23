563e22a6ca4e58e359be7b6a5d8bfd8ffcbfd591
Ali Tafakkor <alitafakkor@gmail.com>
2026-01-23 16:54:52 -0500

    Use SQLite for local development to fix migration error

--- CBS_useman/settings.py
+++ CBS_useman/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = "CBS_useman.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CBS_useman_db',
-        'USER': 'your_username',
-        'PASSWORD': 'your_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
